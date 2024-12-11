3dd62595ef4d91f3ba36289c3f062fee974ba815
Pavlo Suslykov <Pavlo Suslykov>
2024-12-11 09:05:01 -0800

    mlflow 2.19

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import mlflow
 mlflow.set_tracking_uri('https://mlflow.cerbo.stg-gdcorp.tools/')
 
 # Uncomment to load sklearn model
-mlflow_model = mlflow.sklearn.load_model(model_uri=f"models:/propensity-com-usind/8")
+mlflow_model = mlflow.sklearn.load_model(model_uri=f"models:/propensity-m365+obe_bp-us-v1/1")
 
 # Uncomment to load pytorch model
 #mlflow_model = mlflow.pyfunc.load_model(model_uri=f"models:/heuristic-segmentation-m365/4")
